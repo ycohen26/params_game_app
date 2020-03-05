84b8be2044a72953fb20ab159cec404407727a3a
Yossi <yosef.d.cohen@gmail.com>
2020-03-04 18:47:53 -0600

    add first letter method

--- app/controllers/api/params_game_controller.rb
+++ app/controllers/api/params_game_controller.rb
@@ -1,5 +1,10 @@
 class Api::ParamsGameController < ApplicationController
+  # Next make it so that if your name begins with the letter "a", there's a message that appears saying, 
   def name_game
+    @user_name = params["name"]
+    if @user_name[0] == "a"
+      @message = "Hey, your name starts with the first letter of the alphabet!"
+    end
     render 'name_game.json.jb'
   end
 end
